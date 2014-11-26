8f628e9e71f98b3045d77ebfea430da8bc3df72d
Giancarlo Esquivel <gian_13_29@hotmail.com>
2014-11-26 09:55:09 -0500

    creacion de proyecto en codeblogs

---
+++ HolaMundo/include/Clase1.h
@@ -0,0 +1,14 @@
+#ifndef CLASE1_H
+#define CLASE1_H
+
+
+class Clase1
+{
+    public:
+        Clase1();
+        virtual ~Clase1();
+    protected:
+    private:
+};
+
+#endif // CLASE1_H
---
+++ HolaMundo/src/Clase1.cpp
@@ -0,0 +1,11 @@
+#include "Clase1.h"
+
+Clase1::Clase1()
+{
+    //ctor
+}
+
+Clase1::~Clase1()
+{
+    //dtor
+}
